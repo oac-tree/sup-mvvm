370e768dfb5c9678ff134445e91c5e817ed30fa6
Gennady Pospelov <gennady.pospelov@gmail.com>
2022-01-19 12:37:05 +0100

    Add unit tests for new signal/slot

--- tests/testmvvm_model/signalslot.test.cpp
+++ tests/testmvvm_model/signalslot.test.cpp
@@ -37,6 +37,14 @@ public:
   MOCK_METHOD2(onDataChange, void(mvvm::SessionItem* item, int role));
 };
 
+class SlotWidget : public mvvm::Slot
+{
+public:
+  MOCK_METHOD1(onItemDestroy, void(mvvm::SessionItem* item));
+  MOCK_METHOD2(onDataChange, void(mvvm::SessionItem* item, int role));
+};
+
+
 TEST_F(SignalSlotTest, MockWidgetConnectAndDisconnect)
 {
   MockWidget widget;
@@ -58,12 +66,33 @@ TEST_F(SignalSlotTest, MockWidgetConnectAndDisconnect)
   signal(&item);  // perform action
 }
 
+TEST_F(SignalSlotTest, MockWidgetConnectAndLock)
+{
+  MockWidget widget;
+  mvvm::Signal<void(mvvm::SessionItem*)> signal;
+
+  auto connection = signal.connect(&widget, &MockWidget::onItemDestroy);
+
+  mvvm::SessionItem item;
+
+  EXPECT_CALL(widget, onItemDestroy(&item)).Times(1);
+
+  signal(&item);  // perform action
+
+  // disabling connection
+  connection.set_lock(true);
+
+  EXPECT_CALL(widget, onItemDestroy(_)).Times(0);
+  signal(&item);  // perform action
+}
+
 //! Callback container notifies two widgets. Check if one widget is removed,
 //! the second is still notified.
 
 TEST_F(SignalSlotTest, TwoWidgetsNotifiedOneRemoved)
 {
-  MockWidget widget1, widget2;
+  MockWidget widget1;
+  MockWidget widget2;
   mvvm::Signal<void(mvvm::SessionItem*)> signal;
 
   auto connection1 = signal.connect([&](auto item) { widget1.onItemDestroy(item); });
@@ -89,7 +118,8 @@ TEST_F(SignalSlotTest, TwoWidgetsNotifiedOneRemoved)
 
 TEST_F(SignalSlotTest, TwoParameters)
 {
-  MockWidget widget1, widget2;
+  MockWidget widget1;
+  MockWidget widget2;
   mvvm::Signal<void(mvvm::SessionItem*, int)> signal;
 
   auto connection1 = signal.connect([&](auto item, int role) { widget1.onDataChange(item, role); });
@@ -134,8 +164,34 @@ TEST_F(SignalSlotTest, OneSignalTwoWidgetsWithSlotsOneDestroyed)
   // destroying slot
   slot1.reset();
 
+  // only one signal expected
   EXPECT_CALL(widget1, onItemDestroy(&item)).Times(0);
   EXPECT_CALL(widget2, onItemDestroy(&item)).Times(1);
+
+  signal(&item);  // perform action
+}
+
+TEST_F(SignalSlotTest, TwoMockOwnersAndTwoSlots)
+{
+  auto widget1 = std::make_unique<SlotWidget>();
+  auto widget2 = std::make_unique<SlotWidget>();
+  mvvm::Signal<void(mvvm::SessionItem*)> signal;
+
+  signal.connect(widget1.get(), &SlotWidget::onItemDestroy, widget1.get());
+  signal.connect(widget2.get(), &SlotWidget::onItemDestroy, widget2.get());
+
+  mvvm::SessionItem item;
+
+  EXPECT_CALL(*widget1, onItemDestroy(&item)).Times(1);
+  EXPECT_CALL(*widget2, onItemDestroy(&item)).Times(1);
+
+  signal(&item);  // perform action
+
+  // destroying widget2
+  widget2.reset();
+
+  // only one signal expected
+  EXPECT_CALL(*widget1, onItemDestroy(&item)).Times(1);
 
   signal(&item);  // perform action
 }
