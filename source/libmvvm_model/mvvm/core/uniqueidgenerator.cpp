13f0db7feb1f252a23ba78012b8c9acaf9912af4
Gennady Pospelov <gennady.pospelov@gmail.com>
2022-02-02 16:08:03 +0100

    Implement GraphInfoFormatter

--- source/libmvvm_model/mvvm/core/uniqueidgenerator.cpp
+++ source/libmvvm_model/mvvm/core/uniqueidgenerator.cpp
@@ -46,13 +46,6 @@ namespace mvvm
 std::string UniqueIdGenerator::Generate()
 {
   static GeneratorData data;
-  //  std::random_device rd;
-  //  auto seed_data = std::array<int, std::mt19937::state_size> {};
-  //  std::generate(std::begin(seed_data), std::end(seed_data), std::ref(rd));
-  //  std::seed_seq seq(std::begin(seed_data), std::end(seed_data));
-  //  std::mt19937 generator(seq);
-  //  uuids::uuid_random_generator gen{generator};
-
   return uuids::to_string((*data.gen)());
 }
 
