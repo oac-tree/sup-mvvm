c1d80a2e82288722fc9fd8eea335d4b7a03c9e08
Gennady Pospelov <gennady.pospelov@gmail.com>
2022-02-02 16:13:23 +0100

    Implement StatusStringReporter

---
+++ source/libmvvm_view/mvvm/plotting/statusstringreporterfactory.h
@@ -0,0 +1,33 @@
+// ************************************************************************** //
+//
+//  Model-view-view-model framework for large GUI applications
+//
+//! @license   GNU General Public License v3 or higher (see COPYING)
+//! @authors   see AUTHORS
+//
+// ************************************************************************** //
+
+#ifndef MVVM_PLOTTING_STATUSSTRINGREPORTERFACTORY_H
+#define MVVM_PLOTTING_STATUSSTRINGREPORTERFACTORY_H
+
+//! Contains factory methods to create StatusStringReporter
+
+#include "mvvm/view_export.h"
+
+#include <functional>
+#include <memory>
+
+class QCustomPlot;
+
+namespace mvvm
+{
+
+class StatusStringReporter;
+
+//! Creates reporter for status string in QCustomPlot containing graphs.
+MVVM_VIEW_EXPORT std::unique_ptr<StatusStringReporter> CreateGraphReporter(
+    QCustomPlot* custom_plot, std::function<void(const std::string&)> callback);
+
+}  // namespace mvvm
+
+#endif  // MVVM_PLOTTING_STATUSSTRINGREPORTERFACTORY_H
