855d93fa3466ee5d1d6f113d2754880aa199e9b6
Gennady Pospelov <gennady.pospelov@gmail.com>
2022-02-15 15:36:29 +0100

    Implement style namespace for all custom editors

---
+++ source/libmvvm_viewmodel/mvvm/editors/styleutils.cpp
@@ -0,0 +1,33 @@
+// ************************************************************************** //
+//
+//  Model-view-view-model framework for large GUI applications
+//
+//! @license   GNU General Public License v3 or higher (see COPYING)
+//! @authors   see AUTHORS
+//
+// ************************************************************************** //
+
+#include "mvvm/editors/styleutils.h"
+
+namespace mvvm::style
+{
+
+int DefaultPixmapSize()
+{
+  const int default_pixmap_size = 16;
+  return default_pixmap_size;
+}
+
+int DefaultInfoBarHeight()
+{
+  const int default_info_bar_height = 24;
+  return default_info_bar_height;
+}
+
+int DefaultInfoBarTextSize()
+{
+  const int default_info_bar_text_saize = 8;
+  return default_info_bar_text_saize;
+}
+
+}  // namespace mvvm::style
