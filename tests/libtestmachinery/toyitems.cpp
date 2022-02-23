e30eedb8f4634931af5d52e057a8f29d0e569df7
Gennady Pospelov <gennady.pospelov@gmail.com>
2022-02-23 17:02:07 +0100

    Introduce toy items for testing purposes

---
+++ tests/libtestmachinery/toyitems.cpp
@@ -0,0 +1,43 @@
+/******************************************************************************
+ *
+ * Project       : Operational Applications UI Foundation
+ *
+ * Description   : The model-view-viewmodel library of generic UI components
+ *
+ * Author        : Gennady Pospelov (IO)
+ *
+ * Copyright (c) : 2010-2020 ITER Organization,
+ *                 CS 90 046
+ *                 13067 St. Paul-lez-Durance Cedex
+ *                 France
+ *
+ * This file is part of ITER CODAC software.
+ * For the terms and conditions of redistribution or use of this software
+ * refer to the file ITER-LICENSE.TXT located in the top level directory
+ * of the distribution package.
+ *****************************************************************************/
+
+#include "toyitems.h"
+
+namespace TestUtils::ToyItems
+{
+
+LayerItem::LayerItem() : CompoundItem("Layer")
+{
+  AddProperty("Thickness", 42.0);
+  AddProperty("Color", "green");
+}
+
+MultiLayerItem::MultiLayerItem() : CompoundItem(Type)
+{
+  RegisterTag(TagInfo::CreateUniversalTag("Layers", {LayerItem::Type}),
+              /*set_as_default*/ true);
+}
+
+SampleModel::SampleModel() : ApplicationModel("sampleMolde")
+{
+  RegisterItem<LayerItem>();
+  RegisterItem<MultiLayerItem>();
+}
+
+}  // namespace TestUtils::ToyItems
---
+++ tests/libtestmachinery/toyitems.h
@@ -0,0 +1,54 @@
+/******************************************************************************
+ *
+ * Project       : Operational Applications UI Foundation
+ *
+ * Description   : The model-view-viewmodel library of generic UI components
+ *
+ * Author        : Gennady Pospelov (IO)
+ *
+ * Copyright (c) : 2010-2020 ITER Organization,
+ *                 CS 90 046
+ *                 13067 St. Paul-lez-Durance Cedex
+ *                 France
+ *
+ * This file is part of ITER CODAC software.
+ * For the terms and conditions of redistribution or use of this software
+ * refer to the file ITER-LICENSE.TXT located in the top level directory
+ * of the distribution package.
+ *****************************************************************************/
+
+#ifndef TOYITEMS_H
+#define TOYITEMS_H
+
+//! @file toyitems.h
+//! Collection of toy items for testing purpose.
+
+#include "mvvm/model/applicationmodel.h"
+#include "mvvm/model/compounditem.h"
+
+namespace TestUtils::ToyItems
+{
+
+class LayerItem : public CompoundItem
+{
+public:
+  static inline const std::string Type = "Layer";
+  LayerItem();
+};
+
+class MultiLayerItem : public CompoundItem
+{
+public:
+  static inline const std::string Type = "MultiLayer";
+  MultiLayerItem();
+};
+
+class SampleModel : public ApplicationModel
+{
+public:
+  SampleModel();
+};
+
+}  // namespace TestUtils::ToyItems
+
+#endif
