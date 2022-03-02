84d2de8a400fa38bf31e851780030b9b8c2d1304
Gennady Pospelov <gennady.pospelov@gmail.com>
2022-03-02 15:01:57 +0100

    Implement ModelGasChangedController

---
+++ source/libmvvm_model/mvvm/project/modelhaschangedcontroller.h
@@ -0,0 +1,43 @@
+// ************************************************************************** //
+//
+//  Model-view-view-model framework for large GUI applications
+//
+//! @license   GNU General Public License v3 or higher (see COPYING)
+//! @authors   see AUTHORS
+//
+// ************************************************************************** //
+
+#ifndef MVVM_PROJECT_MODELHASCHANGEDCONTROLLER_H
+#define MVVM_PROJECT_MODELHASCHANGEDCONTROLLER_H
+
+#include "mvvm/signals/modellistener.h"
+#include "mvvm/model/applicationmodel.h"
+
+#include <functional>
+
+namespace mvvm
+{
+
+//! Tracks changes in the model.
+//! Allows to check if model has been changed (e.g. modified, inserted or removed items) since last
+//! call of ::resetChanged().
+
+class MVVM_MODEL_EXPORT ModelHasChangedController : public ModelListener<ApplicationModel>
+{
+public:
+  using callback_t = std::function<void()>;
+  ModelHasChangedController(ApplicationModel* model, callback_t callback = {});
+
+  bool hasChanged() const;
+
+  void resetChanged();
+
+private:
+  void process_change();
+  bool m_has_changed{false};
+  callback_t m_callback;  //! informs the user about change in the model
+};
+
+}  // namespace mvvm
+
+#endif  // MVVM_PROJECT_MODELHASCHANGEDCONTROLLER_H
