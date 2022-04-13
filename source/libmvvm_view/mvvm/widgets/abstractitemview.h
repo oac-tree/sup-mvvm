da4023df1227f467d20170c040b92c64f80fbecd
Gennady Pospelov <gennady.pospelov@gmail.com>
2022-04-13 13:52:28 +0200

    Placeholder for AbstractItemView

---
+++ source/libmvvm_view/mvvm/widgets/abstractitemview.h
@@ -0,0 +1,43 @@
+/******************************************************************************
+ *
+ * Project       : Operational Applications UI Foundation
+ *
+ * Description   : The model-view-viewmodel library of generic UI components
+ *
+ * Author        : Gennady Pospelov (IO)
+ *
+ * Copyright (c) : 2010-2022 ITER Organization,
+ *                 CS 90 046
+ *                 13067 St. Paul-lez-Durance Cedex
+ *                 France
+ *
+ * This file is part of ITER CODAC software.
+ * For the terms and conditions of redistribution or use of this software
+ * refer to the file ITER-LICENSE.TXT located in the top level directory
+ * of the distribution package.
+ *****************************************************************************/
+
+#ifndef MVVM_WIDGETS_ABSTRACTITEMVIEW_H
+#define MVVM_WIDGETS_ABSTRACTITEMVIEW_H
+
+#include "mvvm/widgets/itemstreeview.h"
+
+class QAbstractItemView;
+
+namespace mvvm
+{
+
+//! Generic view
+
+class MVVM_VIEW_EXPORT AbstractItemView : public QWidget
+{
+  Q_OBJECT
+
+public:
+  explicit AbstractItemView(QWidget* parent = nullptr);
+  ~AbstractItemView() override;
+};
+
+}  // namespace mvvm
+
+#endif  // MVVM_WIDGETS_ABSTRACTITEMVIEW_H
