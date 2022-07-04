076d551c3178d5eaf1a7ef22fe3a4540b4a5d630
Gennady Pospelov <gennady.pospelov@gmail.com>
2022-07-04 13:58:50 +0200

    Implement prototype of reporting model

---
+++ tests/testmvvm_model/experimental/reporting_model_tests.cpp
@@ -0,0 +1,56 @@
+/******************************************************************************
+ *
+ * Project       : Operational Applications UI Foundation
+ *
+ * Description   : The model-view-viewmodel library of generic UI components
+ *
+ * Author        : Gennady Pospelov (IO)
+ *
+ * Copyright (c) : 2010-2022 ITER Organization,
+ *                 CS 90 046
+ *                 13067 St. Paul-lez-Durance Cedex
+ *                 France
+ *
+ * This file is part of ITER CODAC software.
+ * For the terms and conditions of redistribution or use of this software
+ * refer to the file ITER-LICENSE.TXT located in the top level directory
+ * of the distribution package.
+ *****************************************************************************/
+
+#include "mvvm/experimental/reporting_model.h"
+#include "mvvm/experimental/model.h"
+#include "mvvm/experimental/item.h"
+
+#include <gtest/gtest.h>
+
+using namespace mvvm::experimental;
+
+//! Testing Item.
+
+class ReportingModelTests : public ::testing::Test
+{
+public:
+  std::pair<std::unique_ptr<Item>, Item*> CreateTestData()
+  {
+    auto item = std::make_unique<Item>();
+    auto item_ptr = item.get();
+    return {std::move(item), item_ptr};
+  }
+};
+
+TEST_F(ReportingModelTests, InitialState)
+{
+  ReportingModel<Model> model;
+  EXPECT_NE(model.GetRootItem(), nullptr);
+}
+
+TEST_F(ReportingModelTests, SetData)
+{
+  ReportingModel<Model> model;
+  auto [child, child_ptr] = CreateTestData();
+  auto inserted = model.InsertItem(std::move(child), model.GetRootItem(), 0);
+
+  EXPECT_TRUE(model.SetData(inserted, 42));
+  EXPECT_FALSE(model.SetData(inserted, 42));
+  EXPECT_EQ(inserted->GetData(), variant_t(42));
+}
