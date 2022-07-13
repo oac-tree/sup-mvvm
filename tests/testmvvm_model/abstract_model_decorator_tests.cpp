afd3c2dd3398833ce6754cad2854d9625b9ed2c3
Gennady Pospelov <gennady.pospelov@gmail.com>
2022-07-13 09:26:36 +0200

    Add more tests for AbstractModelDecorator

--- tests/testmvvm_model/abstract_model_decorator_tests.cpp
+++ tests/testmvvm_model/abstract_model_decorator_tests.cpp
@@ -45,13 +45,154 @@ TEST_F(AbstractModelDecoratorTests, InitialState)
 
 TEST_F(AbstractModelDecoratorTests, InsertItem)
 {
+  SessionItem expected_parent;
+  const TagIndex expected_tagindex{"tag", 42};
+
   MockModel mock_model;
-
   auto decorated_model = std::make_unique<MockModelDecorator>(&mock_model);
-
   AbstractModelDecorator decorator(std::move(decorated_model));
 
-  EXPECT_CALL(mock_model, InsertItem(_, _, _)).Times(1);
+  EXPECT_CALL(mock_model, GetType()).Times(0);
+  EXPECT_CALL(mock_model, GetRootItem()).Times(0);
+  EXPECT_CALL(mock_model, InsertItem(_, &expected_parent, expected_tagindex)).Times(1);
+  EXPECT_CALL(mock_model, InsertNewItem(_, _, _)).Times(0);
+  EXPECT_CALL(mock_model, TakeItem(_, _)).Times(0);
+  EXPECT_CALL(mock_model, RemoveItem(_)).Times(0);
+  EXPECT_CALL(mock_model, MoveItem(_, _, _)).Times(0);
+  EXPECT_CALL(mock_model, SetData(_, _, _)).Times(0);
+  EXPECT_CALL(mock_model, Clear(_)).Times(0);
 
-  decorator.InsertItem(std::make_unique<SessionItem>(), decorator.GetRootItem(), {});
+  decorator.InsertItem(std::make_unique<SessionItem>(), &expected_parent, expected_tagindex);
+}
+
+TEST_F(AbstractModelDecoratorTests, InsertNewItem)
+{
+  const std::string expected_type("expected_type");
+  SessionItem expected_parent;
+  const TagIndex expected_tagindex{"tag", 42};
+
+  MockModel mock_model;
+  auto decorated_model = std::make_unique<MockModelDecorator>(&mock_model);
+  AbstractModelDecorator decorator(std::move(decorated_model));
+
+  EXPECT_CALL(mock_model, GetType()).Times(0);
+  EXPECT_CALL(mock_model, GetRootItem()).Times(0);
+  EXPECT_CALL(mock_model, InsertItem(_, _, _)).Times(0);
+  EXPECT_CALL(mock_model, InsertNewItem(_, &expected_parent, expected_tagindex)).Times(1);
+  EXPECT_CALL(mock_model, TakeItem(_, _)).Times(0);
+  EXPECT_CALL(mock_model, RemoveItem(_)).Times(0);
+  EXPECT_CALL(mock_model, MoveItem(_, _, _)).Times(0);
+  EXPECT_CALL(mock_model, SetData(_, _, _)).Times(0);
+  EXPECT_CALL(mock_model, Clear(_)).Times(0);
+
+  decorator.InsertNewItem(expected_type, &expected_parent, expected_tagindex);
+}
+
+TEST_F(AbstractModelDecoratorTests, TakeItem)
+{
+  const std::string expected_type("expected_type");
+  SessionItem expected_parent;
+  const TagIndex expected_tagindex{"tag", 42};
+
+  MockModel mock_model;
+  auto decorated_model = std::make_unique<MockModelDecorator>(&mock_model);
+  AbstractModelDecorator decorator(std::move(decorated_model));
+
+  EXPECT_CALL(mock_model, GetType()).Times(0);
+  EXPECT_CALL(mock_model, GetRootItem()).Times(0);
+  EXPECT_CALL(mock_model, InsertItem(_, _, _)).Times(0);
+  EXPECT_CALL(mock_model, InsertNewItem(_, _, _)).Times(0);
+  EXPECT_CALL(mock_model, TakeItem(&expected_parent, expected_tagindex)).Times(1);
+  EXPECT_CALL(mock_model, RemoveItem(_)).Times(0);
+  EXPECT_CALL(mock_model, MoveItem(_, _, _)).Times(0);
+  EXPECT_CALL(mock_model, SetData(_, _, _)).Times(0);
+  EXPECT_CALL(mock_model, Clear(_)).Times(0);
+
+  decorator.TakeItem(&expected_parent, expected_tagindex);
+}
+
+TEST_F(AbstractModelDecoratorTests, RemoveItem)
+{
+  SessionItem expected_item;
+
+  MockModel mock_model;
+  auto decorated_model = std::make_unique<MockModelDecorator>(&mock_model);
+  AbstractModelDecorator decorator(std::move(decorated_model));
+
+  EXPECT_CALL(mock_model, GetType()).Times(0);
+  EXPECT_CALL(mock_model, GetRootItem()).Times(0);
+  EXPECT_CALL(mock_model, InsertItem(_, _, _)).Times(0);
+  EXPECT_CALL(mock_model, InsertNewItem(_, _, _)).Times(0);
+  EXPECT_CALL(mock_model, TakeItem(_, _)).Times(0);
+  EXPECT_CALL(mock_model, RemoveItem(&expected_item)).Times(1);
+  EXPECT_CALL(mock_model, MoveItem(_, _, _)).Times(0);
+  EXPECT_CALL(mock_model, SetData(_, _, _)).Times(0);
+  EXPECT_CALL(mock_model, Clear(_)).Times(0);
+
+  decorator.RemoveItem(&expected_item);
+}
+
+TEST_F(AbstractModelDecoratorTests, MoveItem)
+{
+  SessionItem expected_item;
+  SessionItem expected_parent;
+  const TagIndex expected_tagindex{"tag", 42};
+
+  MockModel mock_model;
+  auto decorated_model = std::make_unique<MockModelDecorator>(&mock_model);
+  AbstractModelDecorator decorator(std::move(decorated_model));
+
+  EXPECT_CALL(mock_model, GetType()).Times(0);
+  EXPECT_CALL(mock_model, GetRootItem()).Times(0);
+  EXPECT_CALL(mock_model, InsertItem(_, _, _)).Times(0);
+  EXPECT_CALL(mock_model, InsertNewItem(_, _, _)).Times(0);
+  EXPECT_CALL(mock_model, TakeItem(_, _)).Times(0);
+  EXPECT_CALL(mock_model, RemoveItem(_)).Times(0);
+  EXPECT_CALL(mock_model, MoveItem(&expected_item, &expected_parent, expected_tagindex)).Times(1);
+  EXPECT_CALL(mock_model, SetData(_, _, _)).Times(0);
+  EXPECT_CALL(mock_model, Clear(_)).Times(0);
+
+  decorator.MoveItem(&expected_item, &expected_parent, expected_tagindex);
+}
+
+TEST_F(AbstractModelDecoratorTests, SetData)
+{
+  SessionItem expected_item;
+  const int expected_role{42};
+  const variant_t expected_data(std::string("data"));
+
+  MockModel mock_model;
+  auto decorated_model = std::make_unique<MockModelDecorator>(&mock_model);
+  AbstractModelDecorator decorator(std::move(decorated_model));
+
+  EXPECT_CALL(mock_model, GetType()).Times(0);
+  EXPECT_CALL(mock_model, GetRootItem()).Times(0);
+  EXPECT_CALL(mock_model, InsertItem(_, _, _)).Times(0);
+  EXPECT_CALL(mock_model, InsertNewItem(_, _, _)).Times(0);
+  EXPECT_CALL(mock_model, TakeItem(_, _)).Times(0);
+  EXPECT_CALL(mock_model, RemoveItem(_)).Times(0);
+  EXPECT_CALL(mock_model, MoveItem(_, _, _)).Times(0);
+  EXPECT_CALL(mock_model, SetData(&expected_item, expected_data, expected_role)).Times(1);
+  EXPECT_CALL(mock_model, Clear(_)).Times(0);
+
+  decorator.SetData(&expected_item, expected_data, expected_role);
+}
+
+TEST_F(AbstractModelDecoratorTests, Clear)
+{
+  MockModel mock_model;
+  auto decorated_model = std::make_unique<MockModelDecorator>(&mock_model);
+  AbstractModelDecorator decorator(std::move(decorated_model));
+
+  EXPECT_CALL(mock_model, GetType()).Times(0);
+  EXPECT_CALL(mock_model, GetRootItem()).Times(0);
+  EXPECT_CALL(mock_model, InsertItem(_, _, _)).Times(0);
+  EXPECT_CALL(mock_model, InsertNewItem(_, _, _)).Times(0);
+  EXPECT_CALL(mock_model, TakeItem(_, _)).Times(0);
+  EXPECT_CALL(mock_model, RemoveItem(_)).Times(0);
+  EXPECT_CALL(mock_model, MoveItem(_, _, _)).Times(0);
+  EXPECT_CALL(mock_model, SetData(_, _, _)).Times(0);
+  EXPECT_CALL(mock_model, Clear(_)).Times(1);
+
+  decorator.Clear();
 }
