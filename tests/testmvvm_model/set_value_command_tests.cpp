3bd7702f47c9d915383f06ecedd9722c5a4e739c
Gennady Pospelov <gennady.pospelov@gmail.com>
2022-07-22 19:04:35 +0200

    Add test for SetValueCommand executed via NotifyingModelComposer

--- tests/testmvvm_model/set_value_command_tests.cpp
+++ tests/testmvvm_model/set_value_command_tests.cpp
@@ -18,24 +18,34 @@
  *****************************************************************************/
 
 #include "mvvm/commands/set_value_command.h"
+#include "mock_model_event_notifier.h"
 
 #include <gtest/gtest.h>
 #include <mvvm/model/model_composer.h>
+#include <mvvm/model/notifying_model_composer.h>
 #include <mvvm/model/sessionmodel.h>
 #include <mvvm/core/exceptions.h>
 
 using namespace mvvm;
+using ::testing::_;
 
 //! Testing SetValueCommand.
 
 class SetValueCommandTests : public ::testing::Test
 {
 public:
-  std::unique_ptr<ModelComposerInterface> CreateComposer()
+  std::unique_ptr<ModelComposerInterface> CreateStandardComposer()
   {
     return std::make_unique<ModelComposer>(m_model);
   }
+
+  std::unique_ptr<ModelComposerInterface> CreateNotifyingComposer()
+  {
+    return std::make_unique<NotifyingModelComposer<ModelComposer>>(&m_notifier, m_model);
+  }
+
   SessionModel m_model;
+  MockModelEventNotifier m_notifier;
 };
 
 //! Set item value through SetValueCommand command using ModelComposer.
@@ -43,7 +53,7 @@ public:
 TEST_F(SetValueCommandTests, SetValueUsingModelComposer)
 {
   const int role = DataRole::kData;
-  auto composer = CreateComposer();
+  auto composer = CreateStandardComposer();
 
   auto item = m_model.InsertItem<SessionItem>();
 
@@ -75,7 +85,7 @@ TEST_F(SetValueCommandTests, SetValueUsingModelComposer)
 TEST_F(SetValueCommandTests, SetSameValueCommand)
 {
   const int role = DataRole::kData;
-  auto composer = CreateComposer();
+  auto composer = CreateStandardComposer();
 
   // creating an item and setting the data
   auto item = m_model.InsertItem<SessionItem>();
@@ -93,4 +103,42 @@ TEST_F(SetValueCommandTests, SetSameValueCommand)
 
   // undoing command which is in isObsolete state is not possible
   EXPECT_THROW(command->Undo(), mvvm::RuntimeException);
+}
+
+//! Set the value to item
+
+TEST_F(SetValueCommandTests, SetValueUsingNotifyingModelComposer)
+{
+  const int role = DataRole::kData;
+  auto composer = CreateNotifyingComposer();
+
+  auto item0 = m_model.InsertItem<SessionItem>();
+  auto item1 = m_model.InsertItem<SessionItem>();
+  auto item2 = m_model.InsertItem<SessionItem>();
+
+  // initial state
+  EXPECT_FALSE(utils::IsValid(item1->Data(role)));
+
+  // creating command
+  variant_t expected(42);
+  auto command = std::make_unique<SetValueCommand>(composer.get(), item1, expected, role);
+
+  // data is still unchanged
+  EXPECT_FALSE(utils::IsValid(item1->Data(role)));
+
+  EXPECT_CALL(m_notifier, DataChangedNotify(item1, role)).Times(1);
+
+  // executing command
+  command->Execute();
+  EXPECT_TRUE(command->GetResult());  // value was changed
+  EXPECT_EQ(item1->Data(role), expected);
+  EXPECT_FALSE(command->IsObsolete());
+
+  EXPECT_CALL(m_notifier, DataChangedNotify(item1, role)).Times(1);
+
+  // undoing command
+  command->Undo();
+  EXPECT_TRUE(command->GetResult());  // value was changed
+  EXPECT_FALSE(utils::IsValid(item1->Data(role)));
+  EXPECT_FALSE(command->IsObsolete());
 }
