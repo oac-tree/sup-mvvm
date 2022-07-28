9c87dcdeb820d1f7a54ff5b0e3afaf67f0ff51d4
Gennady Pospelov <gennady.pospelov@gmail.com>
2022-07-28 17:12:13 +0200

    Implement unit test for CommandModelComposer::SetData when it is decorating NotifyingModelComposer

--- tests/testmvvm_model/command_model_composer_tests.cpp
+++ tests/testmvvm_model/command_model_composer_tests.cpp
@@ -17,26 +17,39 @@
  * of the distribution package.
  *****************************************************************************/
 
+#include "mock_model_event_notifier.h"
 #include "mvvm/commands/command_model_composer.h"
 
 #include <gtest/gtest.h>
 #include <mvvm/commands/command_stack.h>
 #include <mvvm/model/model_composer.h>
+#include <mvvm/model/notifying_model_composer.h>
 #include <mvvm/model/sessionmodel.h>
 
 using namespace mvvm;
+using ::testing::_;
 
 class CommandModelComposerTests : public ::testing::Test
 {
 public:
+  //! create CommandModelComposer relying on standard ModelComposer
   std::unique_ptr<CommandModelComposer> CreateComposer()
   {
     return std::make_unique<CommandModelComposer>(&m_commands,
                                                   std::make_unique<ModelComposer>(m_model));
   }
 
+  //! CreateModelComposer relying on NotifyingModelComposer
+  std::unique_ptr<CommandModelComposer> CreateNotifyingComposer()
+  {
+    auto notifying_composer =
+        std::make_unique<NotifyingModelComposer<ModelComposer>>(&m_notifier, m_model);
+    return std::make_unique<CommandModelComposer>(&m_commands, std::move(notifying_composer));
+  }
+
   SessionModel m_model;
   CommandStack m_commands;
+  MockModelEventNotifier m_notifier;
 };
 
 TEST_F(CommandModelComposerTests, InitialState)
@@ -176,4 +189,53 @@ TEST_F(CommandModelComposerTests, SetDataSeveralTimes)
   EXPECT_TRUE(m_commands.CanUndo());
   EXPECT_FALSE(m_commands.CanRedo());
   EXPECT_EQ(m_commands.GetIndex(), 2);
+}
+
+//! Set data when composer is capable of notifications.
+TEST_F(CommandModelComposerTests, SetDataWithNotifyingComposer)
+{
+  const int role = DataRole::kData;
+  auto composer = CreateNotifyingComposer();
+
+  // inserting item without data
+  auto item = m_model.InsertItem<SessionItem>();
+  EXPECT_FALSE(utils::IsValid(item->Data(role)));
+
+  // setting data via composer
+  EXPECT_CALL(m_notifier, DataChangedNotify(item, role)).Times(1);
+  variant_t expected_data(42);
+  EXPECT_TRUE(composer->SetData(item, 42, DataRole::kData));
+
+  // checking the data and status of command stack
+  EXPECT_EQ(item->Data(), variant_t(42));
+  EXPECT_TRUE(m_commands.CanUndo());
+  EXPECT_FALSE(m_commands.CanRedo());
+  EXPECT_EQ(m_commands.GetIndex(), 1);
+  EXPECT_EQ(m_commands.GetSize(), 1);
+
+  // undoing the command
+  EXPECT_CALL(m_notifier, DataChangedNotify(item, role)).Times(1);
+  m_commands.Undo();
+
+  // checking the data and status of command stack
+  EXPECT_FALSE(utils::IsValid(item->Data(role)));
+  EXPECT_FALSE(m_commands.CanUndo());
+  EXPECT_TRUE(m_commands.CanRedo());
+  EXPECT_EQ(m_commands.GetIndex(), 0);
+  EXPECT_EQ(m_commands.GetSize(), 1);
+
+  // redoing command
+  EXPECT_CALL(m_notifier, DataChangedNotify(item, role)).Times(1);
+  m_commands.Redo();
+
+  // checking the data and status of command stack
+  EXPECT_EQ(item->Data(), variant_t(42));
+  EXPECT_TRUE(m_commands.CanUndo());
+  EXPECT_FALSE(m_commands.CanRedo());
+  EXPECT_EQ(m_commands.GetIndex(), 1);
+  EXPECT_EQ(m_commands.GetSize(), 1);
+
+  // setting same data and checking absence of notifications
+  EXPECT_CALL(m_notifier, DataChangedNotify(_, _)).Times(0);
+  EXPECT_FALSE(composer->SetData(item, 42, DataRole::kData));
 }
