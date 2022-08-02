744102ae16c3d8cccfc675a360b9582e6add08d8
Gennady Pospelov <gennady.pospelov@gmail.com>
2022-08-02 18:25:47 +0200

    Modernize code

--- examples/dragandmove/dragandmovecore/dragviewmodel.cpp
+++ examples/dragandmove/dragandmovecore/dragviewmodel.cpp
@@ -18,21 +18,25 @@
  *****************************************************************************/
 
 #include "dragviewmodel.h"
+
 #include <mvvm/model/sessionmodel.h>
 #include <mvvm/viewmodel/viewmodel_utils.h>
 #include <mvvm/widgets/widget_utils.h>
+
 #include <QByteArray>
 #include <QDataStream>
 #include <QMimeData>
 #include <algorithm>
 
-namespace {
+namespace
+{
 const QString AppMimeType = "application/org.bornagainproject.moveitem";
-} // namespace
+}  // namespace
 
 using namespace mvvm;
 
-namespace DragAndMove {
+namespace DragAndMove
+{
 
 DragViewModel::DragViewModel(SessionModel* model, QObject* parent)
     : PropertyTableViewModel(model, parent)
@@ -41,64 +45,64 @@ DragViewModel::DragViewModel(SessionModel* model, QObject* parent)
 
 Qt::ItemFlags DragViewModel::flags(const QModelIndex& index) const
 {
-    Qt::ItemFlags defaultFlags = PropertyTableViewModel::flags(index);
-    if (index.isValid())
-        return Qt::ItemIsDragEnabled | Qt::ItemIsDropEnabled | defaultFlags;
-    return Qt::ItemIsDropEnabled | defaultFlags;
+  Qt::ItemFlags defaultFlags = PropertyTableViewModel::flags(index);
+  if (index.isValid())
+    return Qt::ItemIsDragEnabled | Qt::ItemIsDropEnabled | defaultFlags;
+  return Qt::ItemIsDropEnabled | defaultFlags;
 }
 
 QMimeData* DragViewModel::mimeData(const QModelIndexList& index_list) const
 {
   // FIXME restore
-//    auto mimeData = new QMimeData;
-//    auto items = utils::ParentItemsFromIndex(index_list);
+  //    auto mimeData = new QMimeData;
+  //    auto items = utils::ParentItemsFromIndex(index_list);
 
-//    // Saving list of SessionItem's identifiers related to all DemoItem
+  //    // Saving list of SessionItem's identifiers related to all DemoItem
 
-//    QStringList identifiers;
-//    for (auto item : Utils::ParentItemsFromIndex(index_list))
-//        identifiers.append(QString::fromStdString(item->identifier()));
+  //    QStringList identifiers;
+  //    for (auto item : Utils::ParentItemsFromIndex(index_list))
+  //        identifiers.append(QString::fromStdString(item->identifier()));
 
-//    mimeData->setData(AppMimeType, utils::GetByteArray(identifiers));
-//    return mimeData;
+  //    mimeData->setData(AppMimeType, utils::GetByteArray(identifiers));
+  //    return mimeData;
   return {};
 }
 
 Qt::DropActions DragViewModel::supportedDragActions() const
 {
-    return Qt::TargetMoveAction;
+  return Qt::TargetMoveAction;
 }
 
 Qt::DropActions DragViewModel::supportedDropActions() const
 {
-    return Qt::TargetMoveAction;
+  return Qt::TargetMoveAction;
 }
 
 bool DragViewModel::canDropMimeData(const QMimeData* data, Qt::DropAction, int, int,
                                     const QModelIndex&) const
 {
-    return data->hasFormat(AppMimeType);
+  return data->hasFormat(AppMimeType);
 }
 
 bool DragViewModel::dropMimeData(const QMimeData* data, Qt::DropAction action, int row, int column,
                                  const QModelIndex& parent)
 {
   // FIXME restore
-//    if (!canDropMimeData(data, action, row, column, parent))
-//        return false;
+  //    if (!canDropMimeData(data, action, row, column, parent))
+  //        return false;
 
-//    int requested_row = parent.isValid() ? parent.row() : row;
+  //    int requested_row = parent.isValid() ? parent.row() : row;
 
-//    // retrieving list of item identifiers and accessing items
-//    auto identifiers = Utils::deserialize(data->data(AppMimeType));
-//    for (const auto& id : identifiers) {
-//        auto item = sessionModel()->findItem(id.toStdString());
+  //    // retrieving list of item identifiers and accessing items
+  //    auto identifiers = Utils::deserialize(data->data(AppMimeType));
+  //    for (const auto& id : identifiers) {
+  //        auto item = sessionModel()->findItem(id.toStdString());
 
-//        int row = std::clamp(requested_row, 0, item->parent()->itemCount(item->tagRow().tag) - 1);
-//        sessionModel()->moveItem(item, rootSessionItem(), {"", row});
-//    }
+  //        int row = std::clamp(requested_row, 0, item->parent()->itemCount(item->tagRow().tag) -
+  //        1); sessionModel()->moveItem(item, rootSessionItem(), {"", row});
+  //    }
 
-    return false;
+  return false;
 }
 
-} // namespace DragAndMove
+}  // namespace DragAndMove
--- examples/dragandmove/dragandmovecore/dragviewmodel.h
+++ examples/dragandmove/dragandmovecore/dragviewmodel.h
@@ -22,31 +22,34 @@
 
 #include <mvvm/viewmodel/property_table_viewmodel.h>
 
-namespace mvvm {
+namespace mvvm
+{
 class SessionModel;
 }
 
-namespace DragAndMove {
+namespace DragAndMove
+{
 
 //! View model with drag-and-drop support. Relies on PropertyTableViewModel to show
 //! properties of DemoItem in table-like views.
 
-class DragViewModel : public mvvm::PropertyTableViewModel {
-    Q_OBJECT
+class DragViewModel : public mvvm::PropertyTableViewModel
+{
+  Q_OBJECT
 
 public:
-    DragViewModel(mvvm::SessionModel* model, QObject* parent = nullptr);
+  DragViewModel(mvvm::SessionModel* model, QObject* parent = nullptr);
 
-    Qt::ItemFlags flags(const QModelIndex& index) const override;
-    QMimeData* mimeData(const QModelIndexList& index_list) const override;
-    Qt::DropActions supportedDragActions() const override;
-    Qt::DropActions supportedDropActions() const override;
-    bool canDropMimeData(const QMimeData* data, Qt::DropAction, int, int,
-                         const QModelIndex&) const override;
-    bool dropMimeData(const QMimeData* data, Qt::DropAction action, int row, int column,
-                      const QModelIndex& parent) override;
+  Qt::ItemFlags flags(const QModelIndex& index) const override;
+  QMimeData* mimeData(const QModelIndexList& index_list) const override;
+  Qt::DropActions supportedDragActions() const override;
+  Qt::DropActions supportedDropActions() const override;
+  bool canDropMimeData(const QMimeData* data, Qt::DropAction, int, int,
+                       const QModelIndex&) const override;
+  bool dropMimeData(const QMimeData* data, Qt::DropAction action, int row, int column,
+                    const QModelIndex& parent) override;
 };
 
-} // namespace DragAndMove
+}  // namespace DragAndMove
 
-#endif // DRAGANDMOVECORE_DRAGVIEWMODEL_H
+#endif  // DRAGANDMOVECORE_DRAGVIEWMODEL_H
