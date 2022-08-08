43008047692a1e67a1a8e45fff864b4b95ef57dc
Gennady Pospelov <gennady.pospelov@gmail.com>
2022-08-08 14:23:38 +0200

    Change package alias MVVM:: -> sup-mvvm::

--- cmake/modules/installation.cmake
+++ cmake/modules/installation.cmake
@@ -4,45 +4,45 @@
 # https://gitlab.kitware.com/cmake/community/-/wikis/doc/tutorials/Exporting-and-Importing-Targets
 # -----------------------------------------------------------------------------
 
-set(INSTALL_CONFIGDIR ${CMAKE_INSTALL_LIBDIR}/cmake/mvvm)
+set(INSTALL_CONFIGDIR ${CMAKE_INSTALL_LIBDIR}/cmake/sup-mvvm)
 
 # exporting targets to a script and installing it
-install(EXPORT mvvm-targets FILE MVVMTargets.cmake NAMESPACE MVVM:: DESTINATION ${INSTALL_CONFIGDIR})
+install(EXPORT sup-mvvm-targets FILE sup-mvvm-targets.cmake NAMESPACE sup-mvvm:: DESTINATION ${INSTALL_CONFIGDIR})
 
 # -----------------------------------------------------------------------------
 # Exporting
 # -----------------------------------------------------------------------------
 
 # Add all targets to the build-tree export set
-export(TARGETS mvvm_model mvvm_viewmodel mvvm_view NAMESPACE MVVM:: FILE "${PROJECT_BINARY_DIR}/MVVMTargets.cmake")
+export(TARGETS mvvm_model mvvm_viewmodel mvvm_view NAMESPACE sup-mvvm:: FILE "${PROJECT_BINARY_DIR}/sup-mvvm-targets.cmake")
 
 # Export the package for use from the build-tree (goes to $HOME/.cmake)
 set(CMAKE_EXPORT_PACKAGE_REGISTRY ON)
-export(PACKAGE MVVM)
+export(PACKAGE sup-mvvm)
 
 # -----------------------------------------------------------------------------
-# Creating and installing MVVMConfig.cmake
+# Creating and installing sup-mvvm-config.cmake
 # -----------------------------------------------------------------------------
 
 include(CMakePackageConfigHelpers)
 
 # to use in the build tree
-configure_package_config_file(${SUP_MVVM_PROJECT_DIR}/cmake/configs/MVVMConfig.cmake.in
-    ${CMAKE_CURRENT_BINARY_DIR}/MVVMConfig.cmake
+configure_package_config_file(${SUP_MVVM_PROJECT_DIR}/cmake/configs/sup-mvvm-config.cmake.in
+    ${CMAKE_CURRENT_BINARY_DIR}/sup-mvvm-config.cmake
     INSTALL_DESTINATION ${INSTALL_CONFIGDIR}
 )
 
 # to use in install tree
-install(FILES ${CMAKE_CURRENT_BINARY_DIR}/MVVMConfig.cmake DESTINATION ${INSTALL_CONFIGDIR})
+install(FILES ${CMAKE_CURRENT_BINARY_DIR}/sup-mvvm-config.cmake DESTINATION ${INSTALL_CONFIGDIR})
 
 # -----------------------------------------------------------------------------
-# Create and install ConfigVersion.cmake file
+# Create and install sup-mvvm-config-version.cmake file
 # -----------------------------------------------------------------------------
 
-write_basic_package_version_file(${CMAKE_CURRENT_BINARY_DIR}/MVVMConfigVersion.cmake VERSION
+write_basic_package_version_file(${CMAKE_CURRENT_BINARY_DIR}/sup-mvvm-config-version.cmake VERSION
     ${PROJECT_VERSION} COMPATIBILITY AnyNewerVersion)
 
-install(FILES ${CMAKE_CURRENT_BINARY_DIR}/MVVMConfigVersion.cmake DESTINATION ${INSTALL_CONFIGDIR})
+install(FILES ${CMAKE_CURRENT_BINARY_DIR}/sup-mvvm-config-version.cmake DESTINATION ${INSTALL_CONFIGDIR})
 
 
 # -----------------------------------------------------------------------------
