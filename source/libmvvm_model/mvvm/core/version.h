474b8e623fab1708770f2e3643520fbaaff33935
Gennady Pospelov <gennady.pospelov@gmail.com>
2022-10-13 10:56:35 +0200

    Bump version

--- source/libmvvm_model/mvvm/core/version.h
+++ source/libmvvm_model/mvvm/core/version.h
@@ -38,7 +38,7 @@ inline int ProjectVersionMajor()
 //! Returns minor project version.
 inline int ProjectVersionMinor()
 {
-    const int project_version_minor = 2;
+    const int project_version_minor = 3;
     return project_version_minor;
 }
 
@@ -52,7 +52,7 @@ inline int ProjectVersionPatch()
 //! Returns project version string
 inline std::string ProjectVersion()
 {
-    const std::string project_version = "0.2.0";
+    const std::string project_version = "0.3.0";
     return project_version;
 }
 
