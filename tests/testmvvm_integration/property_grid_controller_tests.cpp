5cceed353a242178b2f83bb136c78f103e2949ce
Gennady Pospelov <gennady.pospelov@gmail.com>
2022-10-14 16:40:02 +0200

    Implement unit tests for PropertyViewModel and PropertyTableViewModel

--- tests/testmvvm_integration/property_grid_controller_tests.cpp
+++ tests/testmvvm_integration/property_grid_controller_tests.cpp
@@ -17,9 +17,14 @@
  * of the distribution package.
  *****************************************************************************/
 
+#include "mvvm/viewmodel/property_table_viewmodel.h"
+#include "mvvm/viewmodel/property_viewmodel.h"
 #include "mvvm/widgets/property_grid_controller.h"
 
 #include <gtest/gtest.h>
+#include <mvvm/model/application_model.h>
+#include <mvvm/model/property_item.h>
+#include <mvvm/standarditems/vector_item.h>
 
 #include <QDebug>
 #include <QLabel>
@@ -44,31 +49,34 @@ class PropertyGridControllerTests : public ::testing::Test
 TEST_F(PropertyGridControllerTests, CreateWidget)
 {
   // preparing the model
-  QStandardItemModel model;
-  auto parent_item = model.invisibleRootItem();
+  QStandardItemModel view_model;
+  auto parent_item = view_model.invisibleRootItem();
   QList<QStandardItem*> items = {new QStandardItem("a"), new QStandardItem("b")};
 
   // first item in a row is a label
   items.at(0)->setEditable(false);
   parent_item->insertRow(0, items);
-  // second item in a raw carries an integet
+  // second item in a row carries an integer
   items.at(1)->setData(42, Qt::EditRole);
 
-  PropertyGridController controller(&model);
+  PropertyGridController controller(&view_model);
 
-  auto widget0 = controller.CreateWidget(model.index(0, 0));
-  auto widget1 = controller.CreateWidget(model.index(0, 1));
+  auto widget0 = controller.CreateWidget(view_model.index(0, 0));
+  auto widget1 = controller.CreateWidget(view_model.index(0, 1));
 
   // checking that controller can create proper editors for columns
   EXPECT_NE(dynamic_cast<QLabel*>(widget0.get()), nullptr);
   EXPECT_NE(dynamic_cast<QSpinBox*>(widget1.get()), nullptr);
 }
 
+//! Checking method CreateGrid.
+//! Use QStandardItemModel with two rows and three columns.
+
 TEST_F(PropertyGridControllerTests, CreateGridForStandardModel)
 {
   // preparing the model
-  QStandardItemModel model;
-  auto parent_item = model.invisibleRootItem();
+  QStandardItemModel view_model;
+  auto parent_item = view_model.invisibleRootItem();
   QList<QStandardItem*> row0 = {new QStandardItem("a"), new QStandardItem("b"),
                                 new QStandardItem("b")};
   QList<QStandardItem*> row1 = {new QStandardItem("a"), new QStandardItem("b"),
@@ -76,7 +84,7 @@ TEST_F(PropertyGridControllerTests, CreateGridForStandardModel)
   parent_item->insertRow(0, row0);
   parent_item->insertRow(0, row1);
 
-  PropertyGridController controller(&model);
+  PropertyGridController controller(&view_model);
 
   auto editor_grid = controller.CreateGrid();
 
@@ -84,7 +92,39 @@ TEST_F(PropertyGridControllerTests, CreateGridForStandardModel)
   EXPECT_EQ(editor_grid[0].size(), 3);
 }
 
-// TEST_F(PropertyGridControllerTests, CreateGridForPropertyViewModel)
-//{
+//! Checking method CreateGrid.
+//! Use PropertyViewModel and single VectorItem in it.
 
-//}
+TEST_F(PropertyGridControllerTests, CreateGridForPropertyViewModel)
+{
+  ApplicationModel model;
+  auto parent = model.InsertItem<VectorItem>();
+
+  PropertyViewModel view_model(&model);
+  view_model.SetRootSessionItem(parent);
+
+  PropertyGridController controller(&view_model);
+  auto editor_grid = controller.CreateGrid();
+
+  EXPECT_EQ(editor_grid.size(), 3);
+  EXPECT_EQ(editor_grid[0].size(), 2);
+}
+
+//! Checking method CreateGrid.
+//! Use PropertyTableViewModel with two VectorItem in it.
+
+TEST_F(PropertyGridControllerTests, CreateGridForPropertyTableViewModel)
+{
+  ApplicationModel model;
+  model.InsertItem<VectorItem>();
+  model.InsertItem<VectorItem>();
+
+  PropertyTableViewModel view_model(&model);
+
+  PropertyGridController controller(&view_model);
+  auto editor_grid = controller.CreateGrid();
+
+  EXPECT_EQ(editor_grid.size(), 2);
+  EXPECT_EQ(editor_grid[0].size(), 3);
+}
+
