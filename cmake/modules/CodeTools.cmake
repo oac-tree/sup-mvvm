c216e808c5dc3e637b7643140650aeb927dae6fd
Gennady Pospelov <gennady.pospelov@iter.org>
2023-01-25 16:54:07 +0100

    Switch to finding gtest in CONFIG mode

--- cmake/modules/CodeTools.cmake
+++ cmake/modules/CodeTools.cmake
@@ -57,14 +57,11 @@ endfunction()
 
 # Fetches google test version 1.12.1
 function(fetch_googletest)
-  message(STATUS "GTest with version >= \"1.10\" was not found, fetching from internet" )
   include(FetchContent)
-
   FetchContent_Declare(
     googletest
     GIT_REPOSITORY https://github.com/google/googletest.git
     GIT_TAG 58d77fa8070e8cec2dc1ed015d66b454c8d78850)
-
 FetchContent_MakeAvailable(googletest)
   message(STATUS "GTest binaries are present at ${googletest_BINARY_DIR}")
 endfunction()
