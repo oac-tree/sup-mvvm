30dbdf25e563fa05f2713fc642e86f133f327663
Gennady Pospelov <gennady.pospelov@iter.org>
2023-02-07 17:24:14 +0100

    Cleanup item_connect_utils_tests

--- tests/testmvvm_model/item_connect_utils_tests.cpp
+++ tests/testmvvm_model/item_connect_utils_tests.cpp
@@ -24,7 +24,6 @@
 #include <mvvm/model/application_model.h>
 #include <mvvm/model/sessionitem.h>
 #include <mvvm/model/sessionmodel.h>
-#include <mvvm/signals/signal_slot.h>
 #include <mvvm/standarditems/standard_item_includes.h>
 
 #include <memory>
@@ -42,70 +41,22 @@ public:
 
     void SetItem(mvvm::SessionItem* item)
     {
-      // item inserted
-
-      //      auto on_item_inserted = [this](SessionItem* item, const TagIndex& tag_index)
-      //      { OnItemInserted(item, tag_index); };
-      //      connect::OnItemInserted(item, on_item_inserted, m_slot.get());
-
-      //      auto on_item_inserted_event = [this](const event_variant_t& event) {
-      //      OnItemInserted(event); }; connect::OnItemInserted(item, on_item_inserted_event,
-      //      m_slot.get());
       connect::Connect<ItemInsertedEvent>(item, CreateCallback(), m_slot.get());
       connect::Connect<ItemInsertedEvent>(item, this, &MockWidget::OnEvent, m_slot.get());
       connect::Connect<ItemInsertedEvent>(item, this, &MockWidget::OnConcreteEvent, m_slot.get());
-
-      // about to remove item
-
-      //      auto on_about_to_remove_item = [this](SessionItem* item, const TagIndex& tag_index)
-      //      { OnAboutToRemoveItem(item, tag_index); };
-      //      connect::OnAboutToRemoveItem(item, on_about_to_remove_item, m_slot.get());
-
-      //      auto on_about_to_remove_item_event = [this](const event_variant_t& event)
-      //      { OnAboutToRemoveItem(event); };
-      //      connect::OnAboutToRemoveItem(item, on_about_to_remove_item_event, m_slot.get());
 
       connect::Connect<AboutToRemoveItemEvent>(item, CreateCallback(), m_slot.get());
       connect::Connect<AboutToRemoveItemEvent>(item, this, &MockWidget::OnEvent, m_slot.get());
       connect::Connect<AboutToRemoveItemEvent>(item, this, &MockWidget::OnConcreteEvent,
                                                m_slot.get());
 
-      // item removed
-
-      //      auto on_item_removed = [this](SessionItem* item, const TagIndex& tag_index)
-      //      { OnItemRemoved(item, tag_index); };
-      //      connect::OnItemRemoved(item, on_item_removed, m_slot.get());
-
-      //      auto on_item_removed_event = [this](const event_variant_t& event) {
-      //      OnItemRemoved(event); }; connect::OnItemRemoved(item, on_item_removed_event,
-      //      m_slot.get());
-
       connect::Connect<ItemRemovedEvent>(item, CreateCallback(), m_slot.get());
       connect::Connect<ItemRemovedEvent>(item, this, &MockWidget::OnEvent, m_slot.get());
       connect::Connect<ItemRemovedEvent>(item, this, &MockWidget::OnConcreteEvent, m_slot.get());
 
-      // data changed
-
-      //      auto on_data_changed = [this](SessionItem* item, int role) { OnDataChanged(item,
-      //      role); }; connect::OnDataChanged(item, on_data_changed, m_slot.get());
-
-      //      auto on_data_changed_event = [this](const event_variant_t& event) {
-      //      OnDataChanged(event); }; connect::OnDataChanged(item, on_data_changed_event,
-      //      m_slot.get());
-
       connect::Connect<DataChangedEvent>(item, CreateCallback(), m_slot.get());
       connect::Connect<DataChangedEvent>(item, this, &MockWidget::OnEvent, m_slot.get());
       connect::Connect<DataChangedEvent>(item, this, &MockWidget::OnConcreteEvent, m_slot.get());
-
-      // property changed
-
-      //      auto on_property_changed = [this](SessionItem* item, const std::string& name)
-      //      { OnPropertyChanged(item, name); };
-      //      connect::OnPropertyChanged(item, on_property_changed, m_slot.get());
-
-      //      auto on_property_changed_event = [this](const event_variant_t& event)
-      //      { OnPropertyChanged(event); };
-      //      connect::OnPropertyChanged(item, on_property_changed_event, m_slot.get());
 
       connect::Connect<PropertyChangedEvent>(item, CreateCallback(), m_slot.get());
       connect::Connect<PropertyChangedEvent>(item, this, &MockWidget::OnEvent, m_slot.get());
@@ -122,24 +73,10 @@ public:
       return [this](const mvvm::event_variant_t& arg) { OnCallback(arg); };
     }
 
-    //    MOCK_METHOD(void, OnItemInserted, (mvvm::connect::callback_t));
-    //    MOCK_METHOD(void, OnItemInserted, (const mvvm::event_variant_t& event));
     MOCK_METHOD(void, OnConcreteEvent, (const ItemInsertedEvent& event));
-
-    //    MOCK_METHOD(void, OnAboutToRemoveItem, (SessionItem * item, TagIndex tagindex));
-    //    MOCK_METHOD(void, OnAboutToRemoveItem, (const mvvm::event_variant_t& event));
     MOCK_METHOD(void, OnConcreteEvent, (const AboutToRemoveItemEvent& event));
-
-    //    MOCK_METHOD(void, OnItemRemoved, (SessionItem * item, TagIndex tagindex));
-    //    MOCK_METHOD(void, OnItemRemoved, (const mvvm::event_variant_t& event));
     MOCK_METHOD(void, OnConcreteEvent, (const ItemRemovedEvent& event));
-
-    //    MOCK_METHOD(void, OnDataChanged, (SessionItem * item, int role));
-    //    MOCK_METHOD(void, OnDataChanged, (const mvvm::event_variant_t& event));
     MOCK_METHOD(void, OnConcreteEvent, (const DataChangedEvent& event));
-
-//    MOCK_METHOD(void, OnPropertyChanged, (SessionItem * item, std::string name));
-//    MOCK_METHOD(void, OnPropertyChanged, (const mvvm::event_variant_t& event));
     MOCK_METHOD(void, OnConcreteEvent, (const PropertyChangedEvent& event));
 
     std::unique_ptr<Slot> m_slot;
@@ -182,12 +119,11 @@ TEST_F(ItemConnectUtilsTests, ConvertToPropertyChangedEvent)
     auto result = connect::ConvertToPropertyChangedEvent(compound, event_variant_t(event));
     EXPECT_TRUE(result.has_value());
 
-    PropertyChangedEvent expected_converted_event{compound, "height"};
+    const PropertyChangedEvent expected_converted_event{compound, "height"};
     EXPECT_EQ(result.value(), expected_converted_event);
   }
 
   {  // wrong convertion
-
     // DataChangedEvent happened with property_item can't be PropertyChangedEvent of the same item
     auto result = connect::ConvertToPropertyChangedEvent(property_item, event_variant_t(event));
     EXPECT_FALSE(result.has_value());
@@ -224,8 +160,6 @@ TEST_F(ItemConnectUtilsTests, OnDataChanged)
   const auto expected_role = DataRole::kData;
   const auto expected_item = item;
 
-  //  EXPECT_CALL(widget, OnDataChanged(expected_item, expected_role)).Times(1);
-
   DataChangedEvent expected_event{expected_item, expected_role};
   EXPECT_CALL(widget, OnCallback(event_variant_t(expected_event))).Times(1);
   EXPECT_CALL(widget, OnEvent(event_variant_t(expected_event))).Times(1);
@@ -244,25 +178,20 @@ TEST_F(ItemConnectUtilsTests, OnDataChangedAfterDisconnection)
   item->SetData(42, DataRole::kData);
 
   mock_listener_t widget(item);
-  const auto expected_role = DataRole::kData;
-  const auto expected_item = item;
+  DataChangedEvent expected_event{item, DataRole::kData};
 
-  // expect notification
-  //  EXPECT_CALL(widget, OnDataChanged(expected_item, expected_role)).Times(1);
-
-  DataChangedEvent expected_event{expected_item, expected_role};
   EXPECT_CALL(widget, OnCallback(event_variant_t(expected_event))).Times(1);
   EXPECT_CALL(widget, OnEvent(event_variant_t(expected_event))).Times(1);
   EXPECT_CALL(widget, OnConcreteEvent(expected_event)).Times(1);
 
-  item->SetData(45, expected_role);
+  item->SetData(45, DataRole::kData);
 
   // disconnect widget, expect no notifications
   widget.m_slot.reset();
 
   // no notifications are expected here, strict mock will notify us if it's not the case
 
-  item->SetData(46, expected_role);
+  item->SetData(46, DataRole::kData);
 }
 
 //! Expect no calls OnDataChanged when data is the same.
@@ -274,22 +203,17 @@ TEST_F(ItemConnectUtilsTests, OnDataChangedSameData)
   item->SetData(42, DataRole::kData);
 
   mock_listener_t widget(item);
-  const auto expected_role = DataRole::kData;
-  const auto expected_item = item;
+  DataChangedEvent expected_event{item, DataRole::kData};
 
-  // expect notification
-  //  EXPECT_CALL(widget, OnDataChanged(expected_item, expected_role)).Times(1);
-
-  DataChangedEvent expected_event{expected_item, expected_role};
   EXPECT_CALL(widget, OnCallback(event_variant_t(expected_event))).Times(1);
   EXPECT_CALL(widget, OnEvent(event_variant_t(expected_event))).Times(1);
   EXPECT_CALL(widget, OnConcreteEvent(expected_event)).Times(1);
 
-  item->SetData(45, expected_role);
+  item->SetData(45, DataRole::kData);
 
   // no notifications are expected here, strict mock will notify us if it's not the case
 
-  item->SetData(45, expected_role);
+  item->SetData(45, DataRole::kData);
 }
 
 //! Expect no calls OnDataChanged when other item is changed.
@@ -304,8 +228,7 @@ TEST_F(ItemConnectUtilsTests, OnDataChangedDifferentItem)
 
   mock_listener_t widget(item1);
 
-  // expect no notification
-  //  EXPECT_CALL(widget, OnDataChanged(_, _)).Times(0);
+  // no notifications are expected here, strict mock will notify us if it's not the case
 
   item2->SetData(45, DataRole::kData);  // other item changed
 }
@@ -321,11 +244,8 @@ TEST_F(ItemConnectUtilsTests, OnPropertyChanged)
   item->AddProperty(property_name, 42.0);
 
   mock_listener_t widget(item);
-  const auto expected_item = item;
+  PropertyChangedEvent expected_event{item, property_name};
 
-//  EXPECT_CALL(widget, OnPropertyChanged(expected_item, property_name)).Times(1);
-
-  PropertyChangedEvent expected_event{expected_item, property_name};
   EXPECT_CALL(widget, OnCallback(event_variant_t(expected_event))).Times(1);
   EXPECT_CALL(widget, OnEvent(event_variant_t(expected_event))).Times(1);
   EXPECT_CALL(widget, OnConcreteEvent(expected_event)).Times(1);
@@ -343,11 +263,9 @@ TEST_F(ItemConnectUtilsTests, OnItemInserted)
   compound->RegisterTag(TagInfo::CreateUniversalTag("tag1"), /*set_as_default*/ true);
 
   mock_listener_t widget(compound);
-
   const TagIndex expected_tagindex{"tag1", 0};
-  //  EXPECT_CALL(widget, OnItemInserted(compound, expected_tagindex)).Times(1);
-
   ItemInsertedEvent expected_event{compound, expected_tagindex};
+
   EXPECT_CALL(widget, OnCallback(event_variant_t(expected_event))).Times(1);
   EXPECT_CALL(widget, OnEvent(event_variant_t(expected_event))).Times(1);
   EXPECT_CALL(widget, OnConcreteEvent(expected_event)).Times(1);
@@ -369,17 +287,13 @@ TEST_F(ItemConnectUtilsTests, OnItemRemoved)
 
   mock_listener_t widget(compound);
 
-  //  EXPECT_CALL(widget, OnItemInserted(_, _)).Times(0);
   {
     ::testing::InSequence seq;
-    //    EXPECT_CALL(widget, OnAboutToRemoveItem(compound, expected_tagindex)).Times(1);
 
     AboutToRemoveItemEvent expected_event1{compound, expected_tagindex};
     EXPECT_CALL(widget, OnCallback(event_variant_t(expected_event1))).Times(1);
     EXPECT_CALL(widget, OnEvent(event_variant_t(expected_event1))).Times(1);
     EXPECT_CALL(widget, OnConcreteEvent(expected_event1)).Times(1);
-
-    //    EXPECT_CALL(widget, OnItemRemoved(compound, expected_tagindex)).Times(1);
 
     ItemRemovedEvent expected_event2{compound, expected_tagindex};
     EXPECT_CALL(widget, OnCallback(event_variant_t(expected_event2))).Times(1);
