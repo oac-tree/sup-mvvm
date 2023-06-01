1f231ea2091f5359f560ef69c6d03caabb1021cf
Gennady Pospelov <gennady.pospelov@iter.org>
2023-06-01 15:18:26 +0200

    Make variable names longer to please clang-tidy

--- src/lib/sup-mvvm-model/mvvm/model/item_catalogue.h
+++ src/lib/sup-mvvm-model/mvvm/model/item_catalogue.h
@@ -92,30 +92,30 @@ void ItemCatalogue<T>::RegisterItem(const std::string& item_type, const factory_
 template <typename T>
 bool ItemCatalogue<T>::Contains(const std::string& item_type) const
 {
-  auto it = find_if(m_info.begin(), m_info.end(),
-                    [item_type](auto element) { return element.item_type == item_type; });
-  return it != m_info.end();
+  auto iter = find_if(m_info.begin(), m_info.end(),
+                      [item_type](auto element) { return element.item_type == item_type; });
+  return iter != m_info.end();
 }
 
 template <typename T>
 std::unique_ptr<T> ItemCatalogue<T>::Create(const std::string& item_type) const
 {
-  auto it = find_if(m_info.begin(), m_info.end(),
-                    [item_type](auto element) { return element.item_type == item_type; });
-  if (it == m_info.end())
+  auto iter = find_if(m_info.begin(), m_info.end(),
+                      [item_type](auto element) { return element.item_type == item_type; });
+  if (iter == m_info.end())
   {
     throw KeyNotFoundException("No item registered for model type '" + item_type + "'");
   }
-  return it->factory_func();
+  return iter->factory_func();
 }
 
 template <typename T>
 std::vector<std::string> ItemCatalogue<T>::GetItemTypes() const
 {
   std::vector<std::string> result;
-  for (const auto& x : m_info)
+  for (const auto& info : m_info)
   {
-    result.push_back(x.item_type);
+    result.push_back(info.item_type);
   }
   return result;
 }
@@ -124,9 +124,9 @@ template <typename T>
 std::vector<std::string> ItemCatalogue<T>::GetLabels() const
 {
   std::vector<std::string> result;
-  for (const auto& x : m_info)
+  for (const auto& info : m_info)
   {
-    result.push_back(x.item_label);
+    result.push_back(info.item_label);
   }
   return result;
 }
@@ -142,14 +142,14 @@ int ItemCatalogue<T>::GetItemCount() const
 template <typename T>
 void ItemCatalogue<T>::Merge(const ItemCatalogue& other)
 {
-  for (const auto& it : other.m_info)
+  for (const auto& iter : other.m_info)
   {
-    if (Contains(it.item_type))
+    if (Contains(iter.item_type))
     {
       throw ExistingKeyException("Catalogue contains duplicated records");
     }
 
-    RegisterItem(it.item_type, it.factory_func, it.item_label);
+    RegisterItem(iter.item_type, iter.factory_func, iter.item_label);
   }
 }
 
