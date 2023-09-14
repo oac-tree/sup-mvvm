bf834242754fef45b16a2cae40714aeba12c66e0
Gennady Pospelov <gennady.pospelov@iter.org>
2023-09-14 14:23:09 +0200

    Define supported scalar types in a separate header

---
+++ src/lib/sup-mvvm-model/mvvm/core/basic_scalar_types.cpp
@@ -0,0 +1,31 @@
+/******************************************************************************
+ *
+ * Project       : Operational Applications UI Foundation
+ *
+ * Description   : The model-view-viewmodel library of generic UI components
+ *
+ * Author        : Gennady Pospelov (IO)
+ *
+ * Copyright (c) : 2010-2023 ITER Organization,
+ *                 CS 90 046
+ *                 13067 St. Paul-lez-Durance Cedex
+ *                 France
+ *
+ * This file is part of ITER CODAC software.
+ * For the terms and conditions of redistribution or use of this software
+ * refer to the file ITER-LICENSE.TXT located in the top level directory
+ * of the distribution package.
+ *****************************************************************************/
+
+#include "basic_scalar_types.h"
+
+#include <climits>
+
+namespace mvvm
+{
+
+static_assert(sizeof(int64) == 8, "int64 type needs to have size 8 bytes");
+static_assert(sizeof(uint64) == 8, "uint64 type needs to have size 8 bytes");
+static_assert(sizeof(float64) == 8, "float64 type needs to have size 8 bytes");
+
+}  // namespace mvvm
---
+++ src/lib/sup-mvvm-model/mvvm/core/basic_scalar_types.h
@@ -0,0 +1,38 @@
+/******************************************************************************
+ *
+ * Project       : Operational Applications UI Foundation
+ *
+ * Description   : The model-view-viewmodel library of generic UI components
+ *
+ * Author        : Gennady Pospelov (IO)
+ *
+ * Copyright (c) : 2010-2023 ITER Organization,
+ *                 CS 90 046
+ *                 13067 St. Paul-lez-Durance Cedex
+ *                 France
+ *
+ * This file is part of ITER CODAC software.
+ * For the terms and conditions of redistribution or use of this software
+ * refer to the file ITER-LICENSE.TXT located in the top level directory
+ * of the distribution package.
+ *****************************************************************************/
+
+#ifndef MVVM_CORE_BASIC_SCALAR_TYPES_H_
+#define MVVM_CORE_BASIC_SCALAR_TYPES_H_
+
+/**
+ * @file
+ * Defines basic scalar types supported by the framework.
+ */
+
+namespace mvvm
+{
+
+using boolean = bool;
+using int64 = signed long;
+using uint64 = unsigned long;
+using float64 = double;
+
+}  // namespace mvvm
+
+#endif  // MVVM_CORE_VARIANT_H_
