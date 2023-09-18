ad5b9fd5e0822a113f6037867e3a895823d80619
Gennady Pospelov <gennady.pospelov@iter.org>
2023-09-18 11:52:46 +0200

    Typo in assert text

--- src/lib/sup-mvvm-model/mvvm/core/basic_scalar_types.cpp
+++ src/lib/sup-mvvm-model/mvvm/core/basic_scalar_types.cpp
@@ -36,7 +36,7 @@ static_assert(sizeof(int32) == 4, "int32 type needs to have size 4 bytes");
 static_assert(sizeof(uint32) == 4, "uint32 type needs to have size 4 bytes");
 static_assert(sizeof(int64) == 8, "int64 type needs to have size 8 bytes");
 static_assert(sizeof(uint64) == 8, "uint64 type needs to have size 8 bytes");
-static_assert(sizeof(float32) == 4, "float32 type needs to have size 8 bytes");
+static_assert(sizeof(float32) == 4, "float32 type needs to have size 4 bytes");
 static_assert(sizeof(float64) == 8, "float64 type needs to have size 8 bytes");
 
 }  // namespace mvvm
