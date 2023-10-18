96de887588172348f5aa62d55a2836b51144da3d
Pospelov Gennady <Gennady.Pospelov@iter.org>
2023-10-18 15:33:56 +0200

    Return previous version of ViewModelControllerImpl

---
+++ src/lib/sup-mvvm-viewmodel/mvvm/viewmodel/viewmodel_controller_impl.cpp
@@ -0,0 +1,277 @@
+/******************************************************************************
+ *
+ * Project       : Operational Applications UI Foundation
+ *
+ * Description   : The model-view-viewmodel library of generic UI components
+ *
+ * Author        : Gennady Pospelov (IO)
+ *
+ * Copyright (c) : 2010-2023 ITER Organization,
+ *                 CS 90 046
+ *                 13067 St. Paul-lez-Durance Cedex
+ *                 France
+ *
+ * This file is part of ITER CODAC software.
+ * For the terms and conditions of redistribution or use of this software
+ * refer to the file ITER-LICENSE.TXT located in the top level directory
+ * of the distribution package.
+ *****************************************************************************/
+
+#include "viewmodel_controller_impl.h"
+
+#include <mvvm/core/exceptions.h>
+#include <mvvm/interfaces/children_strategy_interface.h>
+#include <mvvm/interfaces/row_strategy_interface.h>
+#include <mvvm/interfaces/sessionmodel_interface.h>
+#include <mvvm/model/model_utils.h>
+#include <mvvm/model/sessionitem.h>
+#include <mvvm/viewmodel/viewitem_factory.h>
+#include <mvvm/viewmodel/viewmodel_utils.h>
+
+#include <stack>
+
+namespace mvvm
+{
+
+ViewModelControllerImpl::ViewModelControllerImpl(
+    ViewModelBase *viewmodel, std::unique_ptr<ChildrenStrategyInterface> children_strategy,
+    std::unique_ptr<RowStrategyInterface> row_strategy)
+    : m_view_model(viewmodel)
+    , m_children_strategy(std::move(children_strategy))
+    , m_row_strategy(std::move(row_strategy))
+{
+}
+
+ViewModelControllerImpl::~ViewModelControllerImpl() {}
+
+void ViewModelControllerImpl::OnModelEvent(const AboutToInsertItemEvent &event)
+{
+  // nothing to do
+}
+
+void ViewModelControllerImpl::OnModelEvent(const ItemInsertedEvent &event)
+{
+  auto [parent, tag_index] = event;
+
+  auto new_child = parent->GetItem(tag_index);
+
+  int insert_view_index = GetInsertViewIndexOfChild(parent, new_child);
+  auto parent_view = m_view_item_map.FindView(parent);
+
+  if (insert_view_index != -1 && parent_view)
+  {
+    m_view_model->insertRow(parent_view, insert_view_index, CreateTreeOfRows(*new_child));
+  }
+}
+
+void ViewModelControllerImpl::OnModelEvent(const ItemRemovedEvent &event)
+{
+  // nothing to do
+}
+
+void ViewModelControllerImpl::OnModelEvent(const AboutToRemoveItemEvent &event)
+{
+  auto item_to_remove = event.m_item->GetItem(event.m_tag_index);
+
+  if (item_to_remove == GetRootItem() || utils::IsItemAncestor(GetRootItem(), item_to_remove))
+  {
+    // special case when user removes SessionItem which is one of ancestors of our root item
+    // or root item itself
+    m_root_item_path = {};
+    m_view_model->ResetRootViewItem(CreateRootViewItem(nullptr));
+    m_view_item_map.Clear();
+    return;
+  }
+
+  if (auto view = m_view_item_map.FindView(item_to_remove); view)
+  {
+    m_view_model->removeRow(view->parent(), view->row());
+    m_view_item_map.OnItemRemove(item_to_remove);
+  }
+}
+
+void ViewModelControllerImpl::OnModelEvent(const DataChangedEvent &event)
+{
+  for (auto view : utils::FindViewsForItem(m_view_model, event.m_item))
+  {
+    if (isValidItemRole(view, event.m_data_role))
+    {
+      auto index = m_view_model->indexFromItem(view);
+      emit m_view_model->dataChanged(index, index, utils::ItemRoleToQtRole(event.m_data_role));
+    }
+  }
+}
+
+void ViewModelControllerImpl::OnModelEvent(const ModelAboutToBeResetEvent &event)
+{
+  (void)event;
+  // Here we are notified that the model content will be destroyed soon.
+  // To let all views looking at ViewModelBase to perform necessary bookkeeping we have to
+  // emit internal QAbstractViewModel::beginResetModel already now, while `model` content is still
+  // alive.
+  m_view_model->BeginResetModelNotify();
+}
+
+void ViewModelControllerImpl::OnModelEvent(const ModelResetEvent &event)
+{
+  auto custom_root_item = utils::ItemFromPath(*event.m_model, m_root_item_path);
+
+  SessionItem *root_item = custom_root_item ? custom_root_item : event.m_model->GetRootItem();
+  m_root_item_path = utils::PathFromItem(custom_root_item);
+
+  m_view_item_map.Clear();
+  m_view_model->ResetRootViewItem(std::move(CreateTreeOfRows(*root_item, true).at(0)),
+                                  /*notify*/ false);
+  m_view_model->EndResetModelNotify();  //  BeginResetModel was already called
+}
+
+void ViewModelControllerImpl::OnModelEvent(const ModelAboutToBeDestroyedEvent &event)
+{
+  (void)event;
+  m_root_item_path = {};
+  m_view_model->ResetRootViewItem(CreateRootViewItem(nullptr));
+}
+
+const SessionItem *ViewModelControllerImpl::GetRootItem() const
+{
+  return utils::GetItemFromView<SessionItem>(m_view_model->rootItem());
+}
+
+void ViewModelControllerImpl::SetRootItem(SessionItem *root_item)
+{
+  CheckInitialState();
+
+  if (root_item)
+  {
+    m_root_item_path = utils::PathFromItem(root_item);
+    m_view_item_map.Clear();
+    auto root_view_item = std::move(CreateTreeOfRows(*root_item, true).at(0));
+    m_view_model->ResetRootViewItem(std::move(root_view_item));
+  }
+  else
+  {
+    m_view_item_map.Clear();
+    m_root_item_path = {};
+    m_view_model->ResetRootViewItem(CreateRootViewItem(nullptr));
+  }
+}
+
+QStringList ViewModelControllerImpl::GetHorizontalHeaderLabels() const
+{
+  return m_row_strategy->GetHorizontalHeaderLabels();
+}
+
+void ViewModelControllerImpl::CheckInitialState() const
+{
+  if (!m_view_model)
+  {
+    throw RuntimeException("Error in ViewModewlController: viewmodel is absent");
+  }
+
+  if (!m_children_strategy)
+  {
+    throw RuntimeException("Error in ViewModewlController: children strategy is not defined");
+  }
+
+  if (!m_row_strategy)
+  {
+    throw RuntimeException("Error in ViewModewlController: row strategy is not defined");
+  }
+}
+
+int ViewModelControllerImpl::GetInsertViewIndexOfChild(const SessionItem *parent,
+                                                       const SessionItem *child)
+{
+  // children that should get their views
+  auto children = m_children_strategy->GetChildren(parent);
+
+  return utils::IndexOfItem(children, child);
+}
+
+std::vector<std::unique_ptr<ViewItem> > ViewModelControllerImpl::CreateTreeOfRows(SessionItem &item,
+                                                                                  bool is_root)
+{
+  // Every SessionItem will be represented by the vector of ViewItem's.  The first element of this
+  // vector plays the role of parent view for SessionItem's children. So it might contain another
+  // ViewItem vectors.
+
+  // A helper structure to visit SessionItem hierarchy in non iterative manner.
+  struct Node
+  {
+    SessionItem *item{nullptr};    // a SessionItem being visited
+    ViewItem *view_item{nullptr};  // first ViewItem in a row of item's views
+  };
+
+  std::stack<Node> stack;
+
+  std::vector<std::unique_ptr<ViewItem> > row_of_views;
+  if (is_root)
+  {
+    // If the item is marked as root, where is no need to call any strategy to find the row.
+    // There can be only one root item an it will be invisible for trees and tables.
+    row_of_views.push_back(CreateRootViewItem(&item));
+  }
+  else
+  {
+    row_of_views = std::move(m_row_strategy->ConstructRow(&item));
+  }
+
+  if (!row_of_views.empty())
+  {
+    auto *view_item = row_of_views.at(0).get();  // a parent for the following row of views
+    stack.push({&item, view_item});
+  }
+
+  while (!stack.empty())
+  {
+    auto *current_parent = stack.top().item;
+    auto *current_parent_view = stack.top().view_item;
+    stack.pop();
+
+    m_view_item_map.Insert(current_parent, current_parent_view);
+
+    auto children = m_children_strategy->GetChildren(current_parent);
+
+    // visited in reverse order to populate the stack properly
+    for (auto it = children.rbegin(); it != children.rend(); ++it)
+    {
+      auto row = m_row_strategy->ConstructRow(*it);
+
+      if (!row.empty())
+      {
+        auto *next_parent_view = row.at(0).get();
+
+        // Inserting row of views into their parent. We always insert at index 0 to compensate
+        // reverse order.
+        current_parent_view->insertRow(0, std::move(row));
+
+        stack.push({*it, next_parent_view});
+      }
+    }
+  }
+
+  return row_of_views;
+}
+
+ViewItemMap &ViewModelControllerImpl::GetViewItemMap()
+{
+  return m_view_item_map;
+}
+
+//! Returns true if given SessionItem role is valid for view
+
+bool ViewModelControllerImpl::isValidItemRole(const ViewItem *view, int item_role)
+{
+  if (auto presentation = dynamic_cast<const mvvm::SessionItemPresentation *>(view->item());
+      presentation)
+  {
+    if (presentation->GetDataRole() == item_role)
+    {
+      return true;
+    }
+  }
+
+  return item_role == mvvm::DataRole::kAppearance || item_role == mvvm::DataRole::kTooltip;
+}
+
+}  // namespace mvvm
---
+++ src/lib/sup-mvvm-viewmodel/mvvm/viewmodel/viewmodel_controller_impl.h
@@ -0,0 +1,112 @@
+/******************************************************************************
+ *
+ * Project       : Operational Applications UI Foundation
+ *
+ * Description   : The model-view-viewmodel library of generic UI components
+ *
+ * Author        : Gennady Pospelov (IO)
+ *
+ * Copyright (c) : 2010-2023 ITER Organization,
+ *                 CS 90 046
+ *                 13067 St. Paul-lez-Durance Cedex
+ *                 France
+ *
+ * This file is part of ITER CODAC software.
+ * For the terms and conditions of redistribution or use of this software
+ * refer to the file ITER-LICENSE.TXT located in the top level directory
+ * of the distribution package.
+ *****************************************************************************/
+
+#ifndef MVVM_VIEWMODEL_VIEWMODEL_CONTROLLER_IMPL_H_
+#define MVVM_VIEWMODEL_VIEWMODEL_CONTROLLER_IMPL_H_
+
+#include <mvvm/model/path.h>
+#include <mvvm/signals/event_types.h>
+#include <mvvm/viewmodel/i_viewmodel_controller.h>
+#include <mvvm/viewmodel/viewitem_map.h>
+
+#include <QStringList>
+#include <memory>
+
+namespace mvvm
+{
+
+class SessionModelInterface;
+class ViewModelBase;
+class ChildrenStrategyInterface;
+class RowStrategyInterface;
+
+/**
+ * @brief The ViewModelControllerImpl class contains implementation details for ViewModelController.
+ */
+
+class ViewModelControllerImpl : public IViewModelController
+{
+public:
+  explicit ViewModelControllerImpl(ViewModelBase *viewmodel,
+                                   std::unique_ptr<ChildrenStrategyInterface> children_strategy,
+                                   std::unique_ptr<RowStrategyInterface> row_strategy);
+
+  ~ViewModelControllerImpl() override;
+
+  void OnModelEvent(const AboutToInsertItemEvent &event) override;
+
+  void OnModelEvent(const ItemInsertedEvent &event) override;
+
+  void OnModelEvent(const ItemRemovedEvent &event) override;
+
+  void OnModelEvent(const AboutToRemoveItemEvent &event) override;
+
+  void OnModelEvent(const DataChangedEvent &event) override;
+
+  void OnModelEvent(const ModelResetEvent &event) override;
+
+  void OnModelEvent(const ModelAboutToBeResetEvent &event) override;
+
+  void OnModelEvent(const ModelAboutToBeDestroyedEvent &event) override;
+
+  /**
+   * @brief Returns current root item.
+   */
+  const SessionItem *GetRootItem() const override;
+
+  /**
+   * @brief Sets new root item.
+   */
+  void SetRootItem(SessionItem *root_item) override;
+
+  QStringList GetHorizontalHeaderLabels() const override;
+
+  void CheckInitialState() const;
+
+  //! Returns an insert index for a view representing a child.
+  //! Since number of views might not coincide with number of items (some items are marked)
+  //! as hidden, we have to recalculate a view index.
+  int GetInsertViewIndexOfChild(const SessionItem *parent, const SessionItem *child);
+
+  /**
+   * @brief Creates tree of rows with ViewItems representing given SessionItem and all its children.
+   * @param item The item to explore.
+   * @param is_root Item is treated as root item, when false.
+   * @return Vector of ViewItem's containing other ViewItem's
+   *
+   * @details The method visits the item and all its children in non-recursive manner and creates a
+   * tree of rows intended for ViewModel. The tree is formed basing on childred/row strategies.
+   */
+  std::vector<std::unique_ptr<ViewItem>> CreateTreeOfRows(SessionItem &item, bool is_root = false);
+
+  ViewItemMap &GetViewItemMap();
+
+  bool isValidItemRole(const mvvm::ViewItem *view, int item_role);
+
+private:
+  ViewModelBase *m_view_model{nullptr};
+  ViewItemMap m_view_item_map;
+  std::unique_ptr<ChildrenStrategyInterface> m_children_strategy;
+  std::unique_ptr<RowStrategyInterface> m_row_strategy;
+  Path m_root_item_path;  // saves path to custom root item, to restore it on model reset
+};
+
+}  // namespace mvvm
+
+#endif  // MVVM_VIEWMODEL_VIEWMODEL_CONTROLLER_H_
