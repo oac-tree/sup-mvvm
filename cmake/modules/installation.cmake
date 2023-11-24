a7aa7e90a7cf330ca0369bcde74fa5b7b69578b9
Pospelov Gennady <Gennady.Pospelov@iter.org>
2023-11-24 10:26:21 +0100

    Add sup-test to installation

--- cmake/modules/installation.cmake
+++ cmake/modules/installation.cmake
@@ -14,7 +14,7 @@ install(EXPORT sup-mvvm-targets FILE sup-mvvm-targets.cmake NAMESPACE sup-mvvm::
 # -----------------------------------------------------------------------------
 
 # Add all targets to the build-tree export set
-export(TARGETS sup-mvvm-model sup-mvvm-viewmodel sup-mvvm-view NAMESPACE sup-mvvm:: FILE "${PROJECT_BINARY_DIR}/sup-mvvm-targets.cmake")
+export(TARGETS sup-mvvm-model sup-mvvm-viewmodel sup-mvvm-view sup-mvvm-test NAMESPACE sup-mvvm:: FILE "${PROJECT_BINARY_DIR}/sup-mvvm-targets.cmake")
 
 # Export the package for use from the build-tree (goes to $HOME/.cmake)
 if(COA_EXPORT_BUILD_TREE)
