9728710d148af550e8ef84e519adbc8ed8f17c8a
Gennady Pospelov <gennady.pospelov@iter.org>
2025-02-04 13:18:15 +0100

    Modify integer types according to sup-dto

--- src/lib/sup-mvvm-model/mvvm/core/basic_scalar_types.h
+++ src/lib/sup-mvvm-model/mvvm/core/basic_scalar_types.h
@@ -26,21 +26,19 @@
  * sup-dto framework.
  */
 
-#include <cstdint>
-
 namespace mvvm
 {
 
 using boolean = bool;
 using char8 = char;
-using int8 = int8_t;
-using uint8 = uint8_t;
-using int16 = int16_t;
-using uint16 = uint16_t;
-using int32 = int32_t;
-using uint32 = uint32_t;
-using int64 = int64_t;
-using uint64 = uint64_t;
+using int8 = signed char;
+using uint8 = unsigned char;
+using int16 = signed short;
+using uint16 = unsigned short;
+using int32 = signed int;
+using uint32 = unsigned int;
+using int64 = signed long;
+using uint64 = unsigned long;
 using float32 = float;
 using float64 = double;
 
